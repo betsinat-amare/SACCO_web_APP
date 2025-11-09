54109023474f360ca21f26269b35523fc9f153f2
Betsinat Amare <amarebetsinat@gmail.com>
2025-11-09 20:00:44 +0300

    added authmiddleware

--- sacco-app/backend/src/routes/authRoutes.js
+++ sacco-app/backend/src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/me", authMiddleware, async (req, res) => {
+  res.json({
+    message: "User authenticated",
+    user: req.user,
+  });
+});
 export default router;
