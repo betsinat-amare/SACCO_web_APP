61a3bd7b05c46c7d5d17120f03b0b7ee75ee28e3
Betsinat Amare <amarebetsinat@gmail.com>
2025-11-12 13:52:24 +0300

    added saving and credit logic

--- sacco-app/backend/src/index.js
+++ sacco-app/backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import financeRoutes from "./routes/financeRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/finance", financeRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
